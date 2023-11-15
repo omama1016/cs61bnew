df9c413931a703a6c67cbf9fea8aaae111fa9d4d
xyz <along1016@163.com>
2023-11-15 10:30:53 +0800

    lab11 compeleted....

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -9,16 +9,83 @@ public class MazeCycles extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+//    private Maze maze;
+//    public MazeCycles(Maze m) {
+//        super(m);
+//        maze = m;
+//        edgeTo[0] = 0;
+//        distTo[0] = 0;
+//    }
+//
+//    @Override
+//    public void solve() {
+//        // TODO: Your code here!
+//        detectCycleDFS(0);
+//    }
+//
+//    // Helper methods go here
+//    private void detectCycleDFS(int v) {
+//        marked[v] = true;
+//        announce();
+//        for (int w : maze.adj(v)) {
+//            if (marked[w] && edgeTo[v] != w) {
+//                edgeTo[w] = w;
+//                announce();
+//                return;
+//            }
+//
+//            if (!marked[w]) {
+//                edgeTo[w] = v;
+//                announce();
+//                distTo[w] = distTo[v] + 1;
+//                detectCycleDFS(w);
+//            }
+//        }
+//    }
+    /* Inherits public fields:
+    public int[] distTo;
+    public int[] edgeTo;
+    public boolean[] marked;
+    */
+    private int[] parent;
+    private boolean foundCircle = false;
 
     public MazeCycles(Maze m) {
         super(m);
+        parent = new int[maze.V()];
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        dfs(0);
     }
 
-    // Helper methods go here
+    private void dfs(int v) {
+        marked[v] = true;
+        announce();
+
+        for (int w : maze.adj(v)) {
+            if (foundCircle) {
+                return;
+            }
+            if (w != parent[v]) {
+                if (marked[w]) {
+                    edgeTo[w] = v;
+                    announce();
+                    while (v != w) {
+                        int n = parent[v];
+                        edgeTo[v] = n;
+                        announce();
+                        v = n;
+                    }
+                    foundCircle = true;
+                } else {
+                    parent[w] = v;
+                    announce();
+                    dfs(w);
+                }
+            }
+        }
+    }
 }
 
